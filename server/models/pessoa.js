ce9638cae783f3e5206ab22a730848f070fc567c
Vinicius Dantas Januario <vdj@WINDOWS.CIN.UFPE.BR>
2019-08-02 09:00:44 -0300

    password criptografado

--- server/models/pessoa.js
+++ server/models/pessoa.js
@@ -1,4 +1,5 @@
 let axios = require('axios')
+let crypto = require('crypto')
 
 let pers = []
 let logged = []
@@ -14,9 +15,11 @@ function createPer(perInfo) {
         idade: perInfo.idade,
         cpf: perInfo.cpf,
         email: perInfo.email,
-        password: perInfo.password,
         id: persId++
-    } 
+    }
+    let genSalt = saltPassword(perInfo.password)
+    newPer.salt = genSalt.salt
+    newPer.hash = genSalt.passwordData
     pers.push(newPer)
 }
 
@@ -67,6 +70,34 @@ function getPersById (id) {
     } else return null
 }
 
+function findPersByCpf (cpf) {
+    return pers.find(function(per) {
+        per.cpf == cpf
+    })
+}
+
+function getPersByCpf (cpf) {
+    let foundPer = findPersByCpf(cpf)
+
+    if (foundPer) {
+        return foundPer
+    } else return null
+}
+
+function findPersByEmail (email) {
+    return pers.find(function(per) {
+        per.email == email
+    })
+}
+
+function getPersByEmail (email) {
+    let foundPer = findPersByEmail(email)
+
+    if (foundPer) {
+        return foundPer
+    } else return null
+}
+
 function changePass (id, newPass) {
     let toBeUpdated = pers.find(function (per) {
         per.id == id
@@ -85,12 +116,11 @@ function changeName (id, newName) {
     } else return null
 }
 
-function login (body) {
-    let foundPer = findPersById(body.id)
-    if (foundPer) {
-        //crypto
+function login (password, salt, hash) {
+    let verify = sha512(password, salt)
+    if (verify.passwordHash == hash) { 
         logged.push(foundPer)
-    } else return
+    } else return null
 }
 
 function logout (id) {
@@ -101,6 +131,26 @@ function logout (id) {
     if (perIndex != -1) {
         return logged.splice(perIndex, 1)
     } else return null
+}
+
+function randomString (length) {
+    return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0, length)
+}
+
+function sha512 (password, salt){
+    let hash = crypto.createHmac('sha512', salt)
+    hash.update(password)
+    let value = hash.digest('hex')
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+function saltPassword (userPassword) {
+    let salt = randomString(16)
+    let passwordData = sha512(userPassword, salt)
+    return {salt: salt, passwordData: passwordData}
 }
 
 module.exports = {
@@ -115,5 +165,7 @@ module.exports = {
     createPer,
     deletePer,
     login,
-    logout
+    logout,
+    getPersByCpf,
+    getPersByEmail
 }
